c7f8271c675abb69324417dcc6b24f8e70445ef1
seeker.of.light <seekeroflight75@yahoo.com>
2017-02-20 13:11:48 -0500

    added all the communica to app.js

---
+++ server/app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var http = require('http');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json({ typr: '*/*'}));
+var port = process.env.PORT || 3000;
+var server = http.createServer(app);
+
+server.listen(port);
+console.log('Severus is listening on ' + port);
